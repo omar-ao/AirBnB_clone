971a114d62070fdecb94468fda338a2e568e5e13
Omar Ahmed O <deeyndiid@gmail.com>
2024-01-11 12:04:34 +0300

    Add create

--- console.py
+++ console.py
@@ -5,11 +5,31 @@ This module contains the command console for HBnB.
 
 
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """The HBnBCommand interpreter"""
 
     prompt = '(hbnb) '
+
+    class_mapping = {
+            "BaseModel": BaseModel
+            }
+
+    def do_create(self, class_name):
+        """Creates a new instance and saves it to JSON file
+        and prints the instance id
+        """
+        if not class_name:
+            print("** class name missing **")
+            return
+        elif class_name not in self.class_mapping:
+            print("** class doesn't exist **")
+            return
+        obj = self.class_mapping[class_name]()
+        print(obj.id)
+        storage.save()
 
     def do_quit(self, line):
         """Quit command to exit the program"""
