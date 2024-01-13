c1072b2667dba77c97c5548259df48e8ea5ebb31
Omar Ahmed O <deeyndiid@gmail.com>
2024-01-13 14:06:46 +0300

    Correct error message

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ It defines a class FileStorage that serializes instances
 to JSON file and deserializes JSON fiel to instances
 """
 
+
 import json
 import os
 
@@ -44,13 +45,13 @@ class FileStorage:
         JSON file exists
         """
 
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
         from models.amenity import Amenity
+        from models.base_model import BaseModel
+        from models.city import City
+        from models.place import Place
         from models.review import Review
+        from models.state import State
+        from models.user import User
 
         dict_obj = {}
         cls_mapping = {
